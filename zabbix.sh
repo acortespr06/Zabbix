f864d4a82ba84f0ccd75a3bfdbd867deb9eaba3b
Andres Cortes <67211732+acortespr06@users.noreply.github.com>
2023-04-24 23:18:06 -0500

    Update zabbix.sh

--- zabbix.sh
+++ zabbix.sh
@@ -1,34 +1,31 @@
 #!/usr/bin/env bash
 
-# Determine Linux flavor
-if [ -f /etc/redhat-release ]; then
-    OS="RHEL"
-elif [ -f /etc/debian_version ]; then
-    if grep -qi "ubuntu" /etc/issue; then
-        OS="Ubuntu"
-    else
-        OS="Debian"
-    fi
+# Detect Linux distribution and version
+if [ -f /etc/os-release ]; then
+    # Amazon Linux, Red Hat Enterprise Linux, CentOS, Oracle Linux
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # Ubuntu
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
 else
-    echo "Unsupported Linux flavor."
+    echo "Unsupported Linux distribution."
     exit 1
 fi
 
-# Download appropriate Zabbix agent package
-if [ "$OS" == "RHEL" ]; then
-    # RHEL/CentOS
-    yum update
-    yum install wget -y
-    wget https://repo.zabbix.com/zabbix/6.0/rhel/$(uname -m)/zabbix-agent-6.0.0-1.el7.$(uname -m).rpm
-    yum install zabbix-agent-6.0.0-1.el7.$(uname -m).rpm -y
-elif [ "$OS" == "Debian" ] || [ "$OS" == "Ubuntu" ]; then
-    # Debian/Ubuntu
-    wget https://repo.zabbix.com/zabbix/6.0/pool/main/z/zabbix-release/zabbix-release_6.0-1+$(lsb_release -sc)_all.deb
-    dpkg -i zabbix-release_6.0-1+$(lsb_release -sc)_all.deb
-    apt-get update
-    apt-get install zabbix-agent -y
+# Install Zabbix agent
+if [ "$OS" = "Ubuntu" ]; then
+    wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-1+$(lsb_release -cs)_all.deb
+    dpkg -i zabbix-release_6.0-1+$(lsb_release -cs)_all.deb
+    apt update
+    apt -y install zabbix-agent
+elif [[ "$OS" = "Red Hat Enterprise Linux" || "$OS" = "CentOS" ]]; then
+    yum -y install https://repo.zabbix.com/zabbix/6.0/rhel/$VER/x86_64/zabbix-release-6.0-1.el$VER.noarch.rpm
+    yum -y install zabbix-agent
 else
-    echo "Unsupported Linux flavor."
+    echo "Unsupported Linux distribution."
     exit 1
 fi
 
@@ -42,4 +39,3 @@ sed -i "/^Hostname=/ s/$/,$HOSTNAME/g" /etc/zabbix/zabbix_agentd.conf
 
 # Restart Zabbix agent service
 systemctl restart zabbix-agent
-
