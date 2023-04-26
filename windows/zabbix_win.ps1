a50e6b32861d7c8a164ef6b48b7d309d7c636ef8
Andres Cortes <67211732+acortespr06@users.noreply.github.com>
2023-04-26 09:12:33 -0500

    Update zabbix_win.ps1

--- windows/zabbix_win.ps1
+++ windows/zabbix_win.ps1
@@ -1,1 +1,17 @@
+# Copyright (c) 2021-2023 acortes
+# Author: acortes
+# License: MIT
+# Define variables
+
+$ZabbixServer = "zabbix.example.com"
+$ZabbixServerPort = "10050"
+$ZabbixHostname = "$env:COMPUTERNAME"
+
+# Download Zabbix agent MSI installer
+$Url = "https://repo.zabbix.com/zabbix/5.4/windows/x86_64/zabbix_agent-5.4.3-windows-amd64-openssl.msi"
+$Output = "$env:TEMP\zabbix_agent.msi"
+Invoke-WebRequest -Uri $Url -OutFile $Output
+
+# Install Zabbix agent in unattended mode
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$Output`" /qn SERVER=`"$ZabbixServer`" SERVERPORT=`"$ZabbixServerPort`" HOSTNAME=`"$ZabbixHostname`"" -Wait
 
